2171a1dda788d063a0be229798eb14513d20719a
Nathan943 <54563702+Nathan943@users.noreply.github.com>
2021-10-12 21:22:12 -0600

    Pong actually works
    Fixed the ball bouncing, made an enemy paddle, made a somewhat scuffed AI that I will fix later, you can actually play it  partly.

--- VideoGameDesign2122/Assets/Pong scripts/BallBounce.cs
+++ VideoGameDesign2122/Assets/Pong scripts/BallBounce.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class BallBounce : MonoBehaviour
 {
-    //Get rigidbody for movement and have a speed variable
+    //Get rigidbody for movement and have a speed variable as well as direction variable
     Rigidbody2D rb;
     public float ballSpeed = 100;
     Vector2 direction;
+    public Transform ghostball;
+    [HideInInspector] public Transform cloneobj;
+
+    public Ray dirray;
 
     private void Start()
     {
@@ -15,9 +19,15 @@ public class BallBounce : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
 
         //Choose a random direction to go
-        direction = new Vector2(Random.Range(1f, 1f), Random.Range(0f, 0f));
+        direction = new Vector2(Random.Range(0f, 0f), Random.Range(-1f, -1f));
 
         //Move in that direction at Time.deltaTime (Fixed framerate) and multiply by the speed variable
         rb.velocity = direction * ballSpeed * Time.deltaTime;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        cloneobj = Instantiate(ghostball);
+        cloneobj.position = rb.velocity;
     }
 }
--- VideoGameDesign2122/Assets/Pong scripts/RotatePaddle.cs
+++ VideoGameDesign2122/Assets/Pong scripts/RotatePaddle.cs
@@ -13,13 +13,13 @@ public class RotatePaddle : MonoBehaviour
         //If d or right arrow pressed, rotate to the right
         if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow))
         {
-            transform.RotateAround(new Vector3(0f, 0f, 4f), Vector3.back, rotateSpeed * Time.deltaTime);
+            transform.RotateAround(new Vector3(0f, 0f, 0f), Vector3.back, rotateSpeed * Time.deltaTime);
         }
 
         //If a or left arrow pressed, rotate to the left
         if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.RotateAround(new Vector3(0f, 0f, 4f), Vector3.forward, rotateSpeed * Time.deltaTime);
+            transform.RotateAround(new Vector3(0f, 0f, 0f), Vector3.forward, rotateSpeed * Time.deltaTime);
         }
     }
 }
